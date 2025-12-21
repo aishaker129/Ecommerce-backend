73ed23c75ab281c447072d8dc052a9979eb47f51
aishaker129 <aishaker129@gmail.com>
2025-12-22 03:41:42 +0600

    Create new Ecommerce Backend Project

--- Ecommerce-backend/src/main/java/com/ecommerce/EcommerceBackendApplication.java
+++ Ecommerce-backend/src/main/java/com/ecommerce/EcommerceBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 	}
 
